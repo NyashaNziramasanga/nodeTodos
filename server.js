5e762d3096a81be755c5736120a486ff82f69546
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-03 21:10:18 +1100

    Commenting and clean up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-//Would come from a DB
+// Data which would come from a database
 const todos = [
   { id: 1, test: 'Todo One' },
   { id: 2, test: 'Todo Two' },
@@ -9,27 +9,46 @@ const todos = [
 
 // Create a server
 const server = http.createServer((req, res) => {
-  // const { headers, url, method } = req;
-  // console.log(headers, url, method);
+  const { method, url } = req;
 
-  // Simple example
-  // res.write('<h1>Hello World<h1>');
-  // res.end();
-
-  //Returns status code and content type to header
-  //Always specify Content-Type for browser to render
-  // res.setHeader('Content-Type', 'application/json');
-  // res.setHeader('X-Powered-By', 'Node');
-  res.writeHead(200, { 'Content-Type': 'application/json', 'X-Powered-By': 'Node' });
-
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos,
+  let body = [];
+    .on('data', (chunk) => {
+      body.push(chunk);
     })
-  );
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      //Set HTTP status code
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+      };
+
+      //Check HTTP method and url
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+          response.error = 'please add id and text';
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, { 'Content-Type': 'application/json', 'X-Powered-By': 'Node' });
+
+      res.end(JSON.stringify(response));
+    });
 });
 
-// Set port for server
+// Setup port for server
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
